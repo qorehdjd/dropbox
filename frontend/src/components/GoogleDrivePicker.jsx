a0a4840a70bc8bde2d7779741db63337132194cb
남궁혁 <qorehdjd@naver.com>
2024-07-19 16:27:53 +0900

    123

--- frontend/src/components/GoogleDrivePicker.jsx
+++ frontend/src/components/GoogleDrivePicker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const UploadLabel = styled.label`
@@ -14,43 +14,62 @@ const UploadLabel = styled.label`
 `;
 
 const GoogleDrivePicker = ({ onFilesChosen }) => {
+  const [isGapiLoaded, setIsGapiLoaded] = useState(false);
+
   useEffect(() => {
+    const handleClientLoad = () => {
+      window.gapi.load("client:auth2", initClient);
+    };
+
+    const initClient = () => {
+      window.gapi.client
+        .init({
+          apiKey: "AIzaSyA49muLeouNncHAR9vW2TDAMQ7NgiopHwE", // Replace with your API key
+          clientId:
+            "296413793089-ovrgub7ppb03lp3ruabvoruqja3odi2u.apps.googleusercontent.com", // Replace with your client ID
+          scope: "https://www.googleapis.com/auth/drive.readonly",
+          discoveryDocs: [
+            "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+          ],
+        })
+        .then(() => {
+          setIsGapiLoaded(true);
+          const authInstance = window.gapi.auth2.getAuthInstance();
+          if (authInstance.isSignedIn.get()) {
+            console.log("User already signed in.");
+          } else {
+            authInstance.signIn();
+          }
+        });
+    };
+
     const script = document.createElement("script");
     script.src = "https://apis.google.com/js/api.js";
-    script.onload = () => handleClientLoad();
+    script.onload = handleClientLoad;
     document.body.appendChild(script);
+
+    // Cleanup function
+    return () => {
+      const existingScript = document.getElementById("gapi-script");
+      if (existingScript) {
+        document.body.removeChild(existingScript);
+      }
+    };
   }, []);
 
-  const handleClientLoad = () => {
-    window.gapi.load("client:auth2", initClient);
-  };
-
-  const initClient = () => {
-    window.gapi.client
-      .init({
-        apiKey: "AIzaSyA49muLeouNncHAR9vW2TDAMQ7NgiopHwE", // Replace with your API key
-        clientId:
-          "296413793089-ovrgub7ppb03lp3ruabvoruqja3odi2u.apps.googleusercontent.com", // Replace with your client ID
-        scope: "https://www.googleapis.com/auth/drive.readonly",
-        discoveryDocs: [
-          "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
-        ],
-      })
-      .then(() => {
-        console.log("winddd", window.gapi);
-        window.gapi.auth2.getAuthInstance().signIn();
-      });
-  };
-
   const createPicker = () => {
-    const google = window.google;
+    if (!isGapiLoaded) {
+      console.error("GAPI not loaded");
+      return;
+    }
+
     const oauthToken = window.gapi.auth2
       .getAuthInstance()
       .currentUser.get()
       .getAuthResponse().access_token;
 
-    const picker = new google.picker.PickerBuilder()
-      .addView(google.picker.ViewId.DOCS_IMAGES)
+    const picker = new window.google.picker.PickerBuilder()
+      .addView(window.google.picker.ViewId.DOCS_IMAGES)
       .setOAuthToken(oauthToken)
       .setDeveloperKey("AIzaSyA49muLeouNncHAR9vW2TDAMQ7NgiopHwE") // Replace with your API key
       .setCallback(pickerCallback)
@@ -59,11 +78,16 @@ const GoogleDrivePicker = ({ onFilesChosen }) => {
   };
 
   const pickerCallback = (data) => {
-    if (data.action === window.google.picker.Action.PICKED) {
-      const files = data.docs.map((doc) => ({
-        name: doc.name,
-        link: doc.url,
-      }));
+    if (
+      data[window.google.picker.Response.ACTION] ===
+      window.google.picker.Action.PICKED
+    ) {
+      const files = data[window.google.picker.Response.DOCUMENTS].map(
+        (doc) => ({
+          name: doc[window.google.picker.Document.NAME],
+          link: doc[window.google.picker.Document.URL],
+        })
+      );
       onFilesChosen(files);
     }
   };
